83f77a81bb1445c853fe2d46cbfabca542e44378
ketinenis <sketineni@zaizi.com>
2022-01-13 18:07:10 +0000

    modified pipeline.yaml

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -2,4 +2,4 @@
 
 # fly -t set-pipeline -p <pipeline_name> -c <path_to_deploy_file> -l <path_to_deploy_parameters_file>
 
-fly sp -t tutorial set-pipeline -p my-ci -c ./pipeline.yml -l ../parameters.yml
+fly sp -t tutorial set-pipeline -p my-ci -c pipeline.yml -l ./parameters.yml
