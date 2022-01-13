e10ec71255547a12f2ff1cb9534c74814f0d8e2a
ketinenis <sketineni@zaizi.com>
2022-01-13 13:47:02 +0000

    initial commit

---
+++ set-pipeline.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# fly -t set-pipeline -p <pipeline_name> -c <path_to_deploy_file> -l <path_to_deploy_parameters_file>
+
+fly sp -t tutorial set-pipeline -p my-ci -c ./pipeline.yml -l ../parameters.yml
